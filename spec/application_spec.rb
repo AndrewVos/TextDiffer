b966e6f97c829088320c27d63d3698e8166d2597
AndrewVos <andrew.vos@gmail.com>
2010-10-11 22:20:26 +0100

    Added autotest growl notifications

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -12,7 +12,11 @@ describe "TextDiffer" do
     
   describe "GET /" do
     it "renders the index template" do
-      
+      app.new do |erb_app|
+        rendered_html = erb_app.erb :index
+        get "/"
+        last_response.body.should == rendered_html
+      end      
     end
   end
 end
