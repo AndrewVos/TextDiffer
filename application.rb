e4848b3733201b7b0aa9784c056bc2973b669a41
AndrewVos <andrew.vos@gmail.com>
2010-10-11 22:56:00 +0100

    Got the app working very simply

--- application.rb
+++ application.rb
@@ -1,3 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'differ'
+
+get "/?" do
+  erb :index
+end
+
+post "/?" do
+  Differ.format = :html
+  result = Differ.diff(params[:left], params[:right]).to_s
+  erb :index, :locals => {
+    :left => params[:left],
+    :right => params[:right],
+    :result => result
+  }
+end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -9,14 +9,42 @@ describe "TextDiffer" do
   def app
     Sinatra::Application
   end
-    
+
   describe "GET /" do
+    it "does not 404" do
+      get "/"
+      last_response.ok?.should == true
+    end
+
     it "renders the index template" do
       app.new do |erb_app|
         rendered_html = erb_app.erb :index
         get "/"
         last_response.body.should == rendered_html
-      end      
+      end
+    end
+  end
+
+  describe "POST /" do
+    before :each do
+      @left = "hello theeerre!"
+      @right = "and now for something completely different"
+      
+      Differ.format = :html
+      @diffed_text = Differ.diff(@left, @right).to_s
+      post "/", {:left => @left, :right => @right}
+    end
+    
+    it "renders the diffed text" do
+      last_response.body.should include @diffed_text
+    end
+    
+    it "renders the left text" do
+      last_response.body.should include %{<textarea name="left">#{@left}</textarea>}
+    end
+    
+    it "renders the right text" do
+      last_response.body.should include %{<textarea name="right">#{@right}</textarea>}
     end
   end
 end
