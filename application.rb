4c009d049d5b2acd1d9a06e2479660548165f78e
AndrewVos <andrew.vos@gmail.com>
2010-10-12 23:20:35 +0100

    Posting text to / now returns json and populates the divs. Also, the textarea now has the same font applied to it.

--- application.rb
+++ application.rb
@@ -1,11 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 require 'differ'
+require 'json'
 
 get "/?" do
   erb :index
 end
 
 post "/?" do
-  puts params[:left]
+  content_type :json
+  
+  left = params[:left]
+  right = params[:right]
+  left = encode_html(left)
+  right = encode_html(right)
+
+  json = {:left => left, :right => right}.to_json
+end
+
+def encode_html(text)
+  text = text.gsub("\n", "<br/>")
+  text = text.gsub(" ", "&nbsp;")
+  text
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -26,5 +26,36 @@ describe "TextDiffer" do
   end
 
   describe "POST /" do
+    context "with equal left and right" do
+      before :each do
+        @left = "WhatAboutBob?"
+        @right = "WhatAboutBob?"
+        post "/", {:left => @left , :right => @right}
+      end
+
+      it "sets the content type to json" do
+        last_response.content_type.should == "application/json"
+      end
+
+      it "returns the left and right values as json" do
+        expected_response = {:left => @left, :right => @right}.to_json
+        last_response.body.should == expected_response
+      end
+    end
+
+    context "with equal left and right that contain spaces and line breaks" do
+      before :each do
+        @left = "Line breaks\nand spaces!"
+        @right = "Line breaks\nand spaces!"
+        post "/", {:left => @left , :right => @right}
+      end
+
+      it "should replace the line breaks with html breaks and the spaces with non-breaking spaces" do
+        @left = @left.gsub("\n", "<br/>").gsub(" ", "&nbsp;")
+        @right = @right.gsub("\n", "<br/>").gsub(" ", "&nbsp;")
+        expected_response = {:left => @left, :right => @right}.to_json
+        last_response.body.should == expected_response
+      end
+    end
   end
 end
